31ff63816ac3f527f37ec8e5b06256e2d8c1e044
Sumit Binnani <sumit.binnani@gmail.com>
2017-04-25 14:46:05 +0530

    Implemented Model Selectors

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -67,7 +67,6 @@ class SelectorBIC(ModelSelector):
     http://www2.imm.dtu.dk/courses/02433/doc/ch6_slides.pdf
     Bayesian information criteria: BIC = -2 * logL + p * logN
     """
-
     def select(self):
         """ select the best model for self.this_word based on
         BIC score for n between self.min_n_components and self.max_n_components
@@ -76,8 +75,23 @@ class SelectorBIC(ModelSelector):
         """
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection based on BIC scores
-        raise NotImplementedError
+        # implement model selection based on BIC scores
+        best_score, best_model  = float("inf"), None
+        
+        for n_components in range(self.min_n_components, self.max_n_components+1):
+            try:
+                model = self.base_model(n_components)
+                logL = model.score(self.X, self.lengths)
+                n_features = self.X.shape[1]
+                n_params = n_components * (n_components - 1) + 2 * n_features * n_components
+                logN = np.log(self.X.shape[0])
+                bic = -2 * logL + n_params * logN
+                if bic < best_score:
+                    best_score, best_model = bic, model
+            except Exception as e:
+                continue
+    
+        return best_model if best_model is not None else self.base_model(self.n_constant)
 
 
 class SelectorDIC(ModelSelector):
@@ -88,21 +102,87 @@ class SelectorDIC(ModelSelector):
     http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.58.6208&rep=rep1&type=pdf
     DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
     '''
+    
+    models, values = {}, {}
+
+    @classmethod
+    def generate_dictionary(cls, inst):        
+        for n_components in range(inst.min_n_components, inst.max_n_components+1):
+            n_components_models, n_components_ml = {}, {}
+            
+            for word in inst.words.keys():
+                X, lengths = inst.hwords[word]
+                try:
+                    model = GaussianHMM(n_components=n_components, covariance_type="diag", n_iter=1000,
+                                    random_state=inst.random_state, verbose=False).fit(X, lengths)
+                    logL = model.score(X, lengths)
+                    n_components_models[word] = model
+                    n_components_ml[word] = logL
+                except Exception as e:
+                    continue
+            
+            SelectorDIC.models[n_components] = n_components_models
+            SelectorDIC.values[n_components] = n_components_ml
 
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
-
-        # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+        
+        # implement model selection based on DIC scores
+        if len(SelectorDIC.models) == 0:
+            self.generate_dictionary(self)
+        
+        best_score, best_model = float("-inf"), None
+        
+        for n_components in range(self.min_n_components, self.max_n_components + 1):
+            models, ml = SelectorDIC.models[n_components], SelectorDIC.values[n_components]
+            
+            if(self.this_word not in ml):
+                continue
+            
+            avg = np.mean([ml[word] for word in ml.keys() if word != self.this_word])
+            dic = ml[self.this_word] - avg
+            
+            if dic > best_score:
+                best_score, best_model = dic, models[self.this_word]
+                
+        return best_model if best_model is not None else self.base_model(self.n_constant)
 
 
 class SelectorCV(ModelSelector):
     ''' select best model based on average log Likelihood of cross-validation folds
 
     '''
+    
+    n_splits = 3
 
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection using CV
-        raise NotImplementedError
+        # implement model selection using CV
+        best_score, best_model = float("-inf"), None
+        
+        for n_components in range(self.min_n_components, self.max_n_components + 1):
+            scores, n_splits = [], SelectorCV.n_splits
+            model, logL = None, None
+            
+            if(len(self.sequences) < n_splits):
+                break
+            
+            split_method = KFold(random_state=self.random_state, n_splits=n_splits)
+            for cv_train_idx, cv_test_idx in split_method.split(self.sequences):
+                X_train, lengths_train = combine_sequences(cv_train_idx, self.sequences)
+                X_test,  lengths_test  = combine_sequences(cv_test_idx, self.sequences)
+                try:
+                    model = GaussianHMM(n_components=n_components, covariance_type="diag", n_iter=1000,
+                                    random_state=inst.random_state, verbose=False).fit(X_train, lengths_train)
+                    logL = model.score(X_test, lengths_test)
+                    scores.append(logL)
+                except Exception as e:
+                    break
+            
+            avg = np.average(scores) if len(scores) > 0 else float("-inf")
+            
+            if avg > best_score:
+                best_score, best_model = avg, model
+        
+        return best_model if best_model is not None else self.base_model(self.n_constant)
